1b71bd77b853fe16cce1355c3a77066babc03714
Radu <petrtylova.radka@gmail.com>
2026-03-17 19:40:20 +0100

    hlavní stránka + mobil

--- _private/notion-config.php
+++ _private/notion-config.php
@@ -1,5 +1,5 @@
 <?php
 // Notion API Configuration
-define('NOTION_TOKEN', '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');
-define('NOTION_DATABASE_ID', '305268d8d47480bdb676ec9bbbb97f52');
+define('NOTION_TOKEN', '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');
+define('NOTION_DATABASE_ID', '3214da293c8b80ee801ec6b0aa35e58a');
 ?>
